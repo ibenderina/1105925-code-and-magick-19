11ed3eee6a4536d939cd3ce140b0b75af506f246
Irina Lukina <ibenderina@gmail.com>
2020-03-01 13:51:28 +0300

    17. Учебный проект: фильтруй заклинания!

--- js/stat.js
+++ js/stat.js
@@ -1,46 +1,55 @@
 'use strict';
 
 (function () {
-  var MAX_HEIGHT = 150;
-  var WIDTH_OF_BLOCK = 40;
-  var SPACING = 50;
-  var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-  var FIELD_COLOR = '#fff';
   var FONT_STYLE = '16px PT Mono';
-  var FONT_COLOR = '#444444';
-  var WIN_TEXT = 'Ура вы победили!';
-  var RESULT_TEXT = 'Список результатов:';
   var MAIN_NAME = 'Вы';
-  var MAIN_BLOCK_COLOR = 'rgba(255, 0, 0, 1)';
+
+  var Size = {
+    MAX_HEIGHT: 150,
+    WIDTH_OF_BLOCK: 40,
+    SPACING: 50
+  };
+
+  var Color = {
+    SHADOW: 'rgba(0, 0, 0, 0.7)',
+    FIELD: '#fff',
+    FONT: '#444444',
+    MAIN_BLOCK: 'rgba(255, 0, 0, 1)'
+  };
+
+  var Text = {
+    WIN: 'Ура вы победили!',
+    RESULT: 'Список результатов:'
+  };
 
   window.renderStatistics = function (ctx, names, times) {
     var maxCount = getMaxNumber(times);
 
-    ctx.fillStyle = SHADOW_COLOR;
+    ctx.fillStyle = Color.SHADOW;
     ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = FIELD_COLOR;
+    ctx.fillStyle = Color.FIELD;
     ctx.fillRect(100, 10, 420, 270);
     ctx.strokeText = FONT_STYLE;
-    ctx.fillStyle = FONT_COLOR;
-    ctx.fillText(WIN_TEXT, 130, 40);
-    ctx.fillText(RESULT_TEXT, 130, 60);
+    ctx.fillStyle = Color.FONT;
+    ctx.fillText(Text.WIN, 130, 40);
+    ctx.fillText(Text.RESULT, 130, 60);
 
     for (var j = 0; j < names.length; j++) {
       var theirColor = randomColor();
       var ratio = times[j] / maxCount;
       var currentName = names[j];
       var currentTime = times[j];
-      var y = MAX_HEIGHT - ratio * MAX_HEIGHT;
-      var x = 150 + (WIDTH_OF_BLOCK + SPACING) * j;
+      var y = Size.MAX_HEIGHT - ratio * Size.MAX_HEIGHT;
+      var x = 150 + (Size.WIDTH_OF_BLOCK + Size.SPACING) * j;
 
       if (currentName === MAIN_NAME) {
-        ctx.fillStyle = MAIN_BLOCK_COLOR;
+        ctx.fillStyle = Color.MAIN_BLOCK;
       } else {
         ctx.fillStyle = theirColor;
       }
 
-      ctx.fillRect(x, y + 95, WIDTH_OF_BLOCK, ratio * MAX_HEIGHT);
-      ctx.fillStyle = FONT_COLOR;
+      ctx.fillRect(x, y + 95, Size.WIDTH_OF_BLOCK, ratio * Size.MAX_HEIGHT);
+      ctx.fillStyle = Color.FONT;
       ctx.fillText(currentName, x, 265);
       ctx.fillText(Math.floor(currentTime).toString(), x, y + 85);
     }
